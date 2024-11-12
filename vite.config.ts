0572aa430529fc495b22e642d9bc6e88902891fc
Carlos Romero <carlosl70n@yahoo.com>
2024-11-12 13:37:34 -0600

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,18 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 80, // or any port you prefer
+    hmr: {
+      host: "localhost",
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
   },
 });
